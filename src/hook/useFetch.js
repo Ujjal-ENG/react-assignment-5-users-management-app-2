1e64c51ff3481a727b1a6af1c7bcacda5c5fea83
user <ujjalroy7862@gmail.com>
2023-03-05 21:34:33 +0600

    done ass

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -4,33 +4,31 @@ const useFetch = (url) => {
   // Task 1: complete this custom hook
   // step1: create 3 states: data, isLoading, error
 
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(null)
-  
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   // step2: fetch data & handle error
 
   const fetchData = async (url) => {
-
     try {
-      const response = await fetch(url)
-      const data = response.json()
-      setData(data)
-      setIsLoading(false)
-      setError(false)
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+      setIsLoading(false);
+      setError(false);
     } catch (error) {
-      setError(error)
-      setIsLoading(false)
+      setError(error);
+      setIsLoading(false);
     }
 
     useEffect(() => {
-      fetchData(url)
-    }, [])
-    
-  }
+      fetchData(url);
+    }, []);
+  };
 
   // step3: return 3 states
-return {data,isLoading,error}
+  return { data, isLoading, error };
 };
 
 export default useFetch;
